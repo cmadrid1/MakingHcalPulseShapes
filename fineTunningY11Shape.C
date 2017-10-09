30376565abe8be619457042fcc3d20bb35db3db9
Christopher_Madrid <Chris_Madrid@baylor.edu>
2017-10-09 15:16:35 -0500

    fine tuning fineTuningY11Shape

--- fineTunningY11Shape.C
+++ fineTunningY11Shape.C
@@ -56,11 +56,11 @@ TGraph* makeGraph(int num, std::vector<std::vector<double>>& data){
 }
 
 TH1F* makeLandau(double sigma,std::string name){
-  TH1F* gtemp = new TH1F(name.c_str(),"",250,0,249);
-  for(int i = 1; i < 250; i++){
+  TH1F* gtemp = new TH1F(name.c_str(),"",252,0,251);
+  for(int i = 1; i <= 252; i++){
     gtemp->SetBinContent(i,TMath::Landau(i,89+shiftpulses+phase,sigma));
   }
-  gtemp->Scale(1.0/gtemp->Integral(0,250));
+  gtemp->Scale(1.0/gtemp->Integral(0,251));
   return gtemp;
 }
 
@@ -102,8 +102,8 @@ TH1F* computeSiPMShape2017(string name){
     //std::cout<<siPMShape2017_[j]<<","<<endl;
   }
 
-  TH1F* htemp = new TH1F(name.c_str(),"",nBinsSiPM_,0.,nBinsSiPM_);
-  for(unsigned int i = 1; i<nBinsSiPM_; i++){
+  TH1F* htemp = new TH1F(name.c_str(),"",nBinsSiPM_+1,0.,nBinsSiPM_);
+  for(unsigned int i = 1; i<=nBinsSiPM_+1; i++){
     htemp->SetBinContent(i+shiftMC+phase,siPMShape2017_[i]);
   }
   htemp->Scale(nor/htemp->Integral(0,htemp->GetNbinsX()+1));
@@ -111,27 +111,28 @@ TH1F* computeSiPMShape2017(string name){
 }
 
 TH1F* makeY11Histo(string name){
-  TH1F* htemp = new TH1F(name.c_str(),"",nBinsSiPM_,0.,nBinsSiPM_);
-  for(unsigned int i = 1; i<nBinsSiPM_; i++){
-    htemp->SetBinContent(i+shift,Y11TimePDF(i));
+  TH1F* htemp = new TH1F(name.c_str(),"",nBinsSiPM_+1,0.,nBinsSiPM_);
+  for(unsigned int i = 1; i<=nBinsSiPM_+1; i++){
+    htemp->SetBinContent(i+9,Y11TimePDF(i));
   }
-  htemp->Scale(1.0/htemp->Integral(0,htemp->GetNbinsX()+1));
+  htemp->Scale(1.0/htemp->GetBinContent(htemp->GetMaximumBin()));
   return htemp;
 }
 
 TH1F* makePulseHisto(vector<double>& pulse, int bstart, string name){
 	//make pulse template histo (from convolution)
-	TH1F* htempT = new TH1F(name.c_str(),"",250,0,250);
+	TH1F* htempT = new TH1F(name.c_str(),"",252,0,251);
 	for(unsigned b = 0; b < pulse.size(); ++b){
-		htempT->SetBinContent(b+bstart,pulse[b]);
+	  htempT->SetBinContent(b+bstart,pulse[b]);
 	}
 	htempT->Scale(nor/htempT->Integral(0,htempT->GetNbinsX()+1));
 	return htempT;
 }
 
-void fineTunningY11Shape(){
+//void fineTunningY11Shape(){
+int main(){
   TCanvas *c0 = new TCanvas("PulseShape_203","",1000,800);  
-  TLegend* catLeg0 = new TLegend(0.73,0.65,0.99,0.9);
+  TLegend* catLeg0 = new TLegend(0.7,0.75,0.99,0.9);
   gPad->SetTopMargin(0.1);
   gPad->SetBottomMargin(0.15);
   gPad->SetRightMargin(0.05);
@@ -162,36 +163,7 @@ void fineTunningY11Shape(){
   catLeg0->SetTextSize(0.04);
   catLeg0->AddEntry(h203_MC,"New MC","l");
   catLeg0->Draw();
-
-  //TGraph* DeConvData = NULL;
-  //DeConvData = makeGraph(17,deConvData);
-  //DeConvData->SetMarkerColor(kGreen+2);
-  //DeConvData->SetLineColor(kGreen+2);
-  //DeConvData->Draw("P same");
-  //catLeg0->AddEntry(DeConvData,"Deconvolved Data","P");
-  //
-  //TF1 *fa2 = new TF1("fa2","[0]*(1-exp(-(x-9)/0.44064))*exp(-(x-9)/10.0186) + [1]*TMath::Landau(x,[2]-9,[3])",0,250);
-  //fa2->SetParLimits(0,0,15);
-  //fa2->SetParLimits(1,1,15);
-  //fa2->SetParLimits(2,0,15);
-  //fa2->SetParLimits(3,5,15);
-  //fa2->SetParameter(0,5.84053);
-  //fa2->SetParameter(1,10.3613);
-  //fa2->SetParameter(2,2);
-  //fa2->SetParameter(3,2);
-  ////NewData->Fit(fa2,"N","",0,50);
-  //DeConvData->Fit(fa2,"N","",0,60);
-  ////DeConvDataSpline->Fit(fa2,"N","",0,60);
-  //fa2->SetLineColor(kBlue);
-  //catLeg0->AddEntry(fa2,"Deconvolved Fit: Exp + Landau","l");
-  //fa2->Draw("same");
   
-  TH1F* Y11_MC = NULL;
-  Y11_MC = makeY11Histo("Y11_MC");
-  Y11_MC->SetLineColor(4);
-  //Y11_MC->Draw("same hist");
-  //catLeg0->AddEntry(Y11_MC,"Y11 (New FSU)","l");
-
   TH1F* h205_pulses = NULL;
   h205_pulses = makePulseHisto(s205,69+shiftpulses+phase,"205");
   h205_pulses->SetLineColor(kBlue);
@@ -199,29 +171,82 @@ void fineTunningY11Shape(){
   //catLeg0->AddEntry(h205_pulses,"205","l");
 
   TH1F* new205 = NULL;
-  new205 = makeLandau(4.57,"new 205");
+  new205 = makeLandau(4.57,"New 205");
   new205->Draw("hist same");
   new205->SetLineColor(kBlue);
-  catLeg0->AddEntry(new205,"Data");
+  catLeg0->AddEntry(new205,"New 205");
 
   TH1F* h203_pulses = NULL;
   h203_pulses = makePulseHisto(s203,65+shiftpulses+phase,"203");
   h203_pulses->SetLineColor(kBlack);
   h203_pulses->Draw("same hist");
   catLeg0->AddEntry(h203_pulses,"Current MC","l");
+
+  TCanvas *c1 = new TCanvas("PulseShape_Y11","",1000,800);  
+  TLegend* catLeg1 = new TLegend(0.55,0.65,0.99,0.9);
+  gPad->SetTopMargin(0.1);
+  gPad->SetBottomMargin(0.15);
+  gPad->SetRightMargin(0.05);
+  gPad->SetLeftMargin(0.16);
+  //gPad->SetLogy();
+
+  TH1F* Y11_MC = NULL;
+  Y11_MC = makeY11Histo("Y11_MC");
+  Y11_MC->SetMinimum(0.00000001);
+  Y11_MC->SetMaximum(1.1);
+  Y11_MC->GetXaxis()->SetRange(0,100);
+  Y11_MC->GetXaxis()->SetTitle("time (ns)");
+  Y11_MC->GetYaxis()->SetTitle("A.U.");
+  Y11_MC->SetTitle("Pulse Shape Comparison");
+  Y11_MC->SetName("Pulse Shape  Comparison");
+  Y11_MC->SetTitleSize(0.002);
+  Y11_MC->SetTitleSize(0.05,"X");
+  Y11_MC->SetTitleSize(0.04,"Y");
+  Y11_MC->SetTitleOffset(1.2,"X");
+  Y11_MC->SetTitleOffset(1.8,"Y");
+  Y11_MC->SetLabelSize(0.05,"X");
+  Y11_MC->SetLabelSize(0.05,"Y");
+  Y11_MC->SetStats(false);
+  Y11_MC->SetLineColor(kRed);
+  Y11_MC->Draw("hist");
+  catLeg1->SetBorderSize(0);
+  catLeg1->SetFillStyle(0);
+  catLeg1->SetTextSize(0.04);
+  catLeg1->AddEntry(Y11_MC,"Y11 + Landau","l");
+  catLeg1->Draw();
+
+  TGraph* DeConvData = NULL;
+  DeConvData = makeGraph(17,deConvData);
+  DeConvData->SetMarkerColor(kGreen+2);
+  DeConvData->SetLineColor(kGreen+2);
+  DeConvData->Draw("P same");
+  
+  TF1 *fa2 = new TF1("fa2","[0]*(1-exp(-(x-9)/0.44064))*exp(-(x-9)/10.0186) + [1]*TMath::Landau(x,[2]-9,[3])",0,250);
+  fa2->SetParLimits(0,0,15);
+  fa2->SetParLimits(1,0,15);
+  fa2->SetParLimits(2,0,15);
+  fa2->SetParLimits(3,5,15);
+  fa2->SetParameter(0,5.84053);
+  fa2->FixParameter(1,0.0);
+  fa2->SetParameter(2,2);
+  fa2->SetParameter(3,2);
+  DeConvData->Fit(fa2,"NQ","",0,60);
+  fa2->SetLineColor(kBlue);
+  catLeg1->AddEntry(fa2,"Deconvolved Fit: Exp","l");
+  fa2->Draw("same");
+  catLeg1->AddEntry(DeConvData,"Deconvolved Data","P");
   
   std::cout<<"203Root:  "<<h203_MC->GetMaximumBin()<<endl;
   //std::cout<<"203pulses.h:  "<<h203_pulses->GetMaximumBin()<<endl;
   std::cout<<"new205:  "<<new205->GetMaximumBin()<<endl;
-  //std::cout<<"Testbeam:    "<<highCharge->GetMaximumBin()<<endl;
   //std::cout<<"Y11 New:    "<<Y11_MC->GetMaximumBin()<<endl;
 
   //hand203new->Print("all");
   //hand203old->Print("all");
   //hand205->Print("all");
 
-  //c0->SaveAs("log203comparison.pdf");
-  //c1->SaveAs("hand_integrated.pdf");
+  c0->SaveAs("203_new205_NewY11comparison.pdf");
+  c1->SaveAs("Y11Comparison.pdf");
   //h203_MC->Print("all");
   //h203_pulses->Print("all");
   //h205_pulses->Print("all");
